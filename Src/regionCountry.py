68cf6db8f707ecdf48d310f0a48119de33227bd0
Santiago Gonzalez <saagonzalezp@correo.udistrital.edu.co>
2021-01-31 16:35:02 -0500

    Tercer Commit: Actualizacion

--- Src/regionCountry.py
+++ Src/regionCountry.py
@@ -6,8 +6,7 @@ from timeit import default_timer
 class regionCountry:
 
     def __init__(self):
-        self.timeStart = default_timer()
-
+        
         self.headers = {
             'x-rapidapi-key': "80d4ed17c2msh66ebb8cd037c572p11e422jsna3e2d005ae38",
             'x-rapidapi-host': "restcountries-v1.p.rapidapi.com"
@@ -18,6 +17,8 @@ class regionCountry:
         return response.json()
 
     def regions(self):
+        
+        self.timeStart = default_timer()
         #res = self.petition("https://restcountries-v1.p.rapidapi.com/all")
         res = self.petition("https://restcountries.eu/rest/v2/all?fields=region")    
 
@@ -27,6 +28,8 @@ class regionCountry:
             if (i['region'] not in regions) and (i['region'] not in '') :
                 regions.append(i['region'])
 
+        self.timeFinal = default_timer()
+        self.generalTime = self.timeFinal-self.timeStart
         return regions        
 
     def hash(self,msj):
@@ -34,12 +37,14 @@ class regionCountry:
 
     def countrys(self,regions):
         countrys = []
-
+        
         for i in regions:
+            self.timeStart = default_timer() 
             res = self.petition("https://restcountries.eu/rest/v2/region/"+str(i)+"?fields=name;languages")
             city = random.randint(0,(len(res)-1))
-            timeFinal = default_timer()
-            time = timeFinal-self.timeStart
+            self.timeFinal = default_timer()
+            timeIndependent = self.timeFinal-self.timeStart
+            time = self.generalTime+timeIndependent
             limit_time = float("%.5f" %time)
             country = {'region': i, 'country': res[city]['name'], 'language': self.hash(str(res[city]['languages'][0]['name'])), 'time':limit_time}
             countrys.append(country)
